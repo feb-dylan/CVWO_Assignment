97f4b5daecea8ee359519e907c2c776dc8da497c
feb-dylan <painghtoo9789@gmail.com>
2026-03-07 21:11:31 +0630

    port to env and db variable changes updated

--- forum-backend/main.go
+++ forum-backend/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"forum/config"
 	"forum/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	config.ConnectDB()
+	db := config.ConnectDB()
 
 	r := gin.Default()
 
-	routes.RegisterRoutes(r, config.DB)
+	routes.RegisterRoutes(r, db)
 
-	port := ":8080"
-	r.Run(port)
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+	log.Printf("Starting server on port %s", port)
+	r.Run(":" + port)
 }
